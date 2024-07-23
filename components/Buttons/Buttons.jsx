68f2626e1f1f0ac9aba59e1337ebfb914fff8d26
SalauDml <damolasalau23@gmail.com>
2024-07-23 20:31:22 +0100

    added switch conditional for choosing class

---
+++ components/Buttons/Buttons.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+function Buttons({type,text}) {
+  let cn = ''
+  switch(type) {
+    case 'nav':
+      cn = 'Buttonnav'
+      break;
+    case 'herotransparent':
+      cn ='Buttonherotransparent'
+      break;
+    case 'listingbutton':
+      cn = 'Buttonlisting'
+      break;
+  
+  
+  }
+
+  return (
+    <button className= {styles[cn]} >{text}</button>
+  )
+}
+
+export default Buttons
+// navbar button, hero button, listing button
+// nav - white bg black text
+// hero - one nav, one transparent bg, white text, white border
+// listing button - transparent bg black text black border
+
